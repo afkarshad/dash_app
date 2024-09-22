d1760db18c8ba917fdcc94fa0cd293099df717c0
Afkar <mursil.afkar@tokopedia.com>
2024-09-22 10:56:43 +0700

    add app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+from dash import Dash, html, dcc, callback, Output, Input, dash_table
+import plotly.express as px
+import pandas as pd
+
+df = pd.read_csv('https://raw.githubusercontent.com/afkarshad/dash_app/refs/heads/main/digital_wallet_transactions.csv')
+
+app = Dash(__name__)
+
+app.layout = html.Div([
+    html.Div(children='My First App with Data'),
+    dash_table.DataTable(data=df.to_dict('records'), page_size=10)
+])
+
+if __name__ == '__main__':
+    app.run(debug=True)
